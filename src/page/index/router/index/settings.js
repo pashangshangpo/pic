aef4a46473eb902c4aaaaff79de7739ddf717295
爬上上坡 <pashangshangpo@gmail.com>
2018-06-02 15:42:01 +0800

    init settings

---
+++ src/page/index/router/index/settings.js
@@ -0,0 +1,17 @@
+/**
+ * @file 设置
+ * @author pashangshangpo
+ * @createTime 2018年6月2日 下午3:39
+ */
+
+import React, { Component } from 'react'
+import { el } from 'pssp/util'
+
+export default class extends Component {
+  render() {
+    return el(
+      'div',
+      {}
+    )
+  }
+}
