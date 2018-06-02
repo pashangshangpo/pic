e691460eb6ed40891930856e537a65912aed40bd
爬上上坡 <pashangshangpo@gmail.com>
2018-06-02 12:31:46 +0800

    init index page

--- src/page/index/router/index/index.js
+++ src/page/index/router/index/index.js
@@ -5,11 +5,60 @@
  */
 
 import React, { Component } from 'react'
+import { el } from 'pssp/util'
+import { Layout } from 'pssp-pc'
+
+const { Sider, Menu, Content } = Layout
 
 export default class extends Component {
+  renderContent = () => {
+    return el(
+      Content,
+      {
+        style: {
+          padding: '12px'
+        }
+      },
+      '我是内容内容'
+    )
+  }
+
+  renderSider = () => {
+    return el(
+      Sider,
+      {},
+      el(
+        Menu,
+        {
+          mode: 'vertical',
+          checked: 'gitee',
+          data: [
+            {
+              name: 'gitee'
+            },
+            {
+              name: '设置'
+            }
+          ],
+          onClick: (item, section, data) => {
+          },
+          onOpenChange: (item, data) => {
+          }
+        }
+      )
+    )
+  }
+
   render() {
-    return (
-      <div>123123</div>
+    return el(
+      Layout,
+      {
+        style: {
+          backgroundColor: '#fff'
+        }
+      },
+      this.renderSider(),
+      this.renderContent()
     )
   }
 }
