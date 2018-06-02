50b916e469a10dc1139a2f82537bec1a82db97fd
爬上上坡 <pashangshangpo@gmail.com>
2018-06-02 16:56:38 +0800

    根据配置来进行上传图片

--- src/page/index/router/index/uploadPic.js
+++ src/page/index/router/index/uploadPic.js
@@ -6,32 +6,26 @@
 
 import { clipboard, ipcRenderer } from 'electron'
 
-ipcRenderer.on('toUploadPic', () => {
-  const base64 = clipboard.readImage().toDataURL().split(',')[1]
-  const gitee = localStorage.getItem('gitee')
+const picServer = {
+  gitee: (config, content, path) => {
+    const { accessToken, owner, repo } = config
 
-  if (gitee) {
-    const { accessToken, owner, repo } = JSON.parse(gitee)
-
-    if (base64) {
-      const path = `pic-${Date.now()}-${Math.random().toString(32)}.png`
-
-      fetch(
-        `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${path}`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          // 需要设置Content-Type 否则报401错误
-          headers: {
-            'Content-Type': 'application/json;charset=UTF-8'
-          },
-          body: JSON.stringify({
-            access_token: accessToken,
-            content: base64,
-            message: `uploadpic ${path}`
-          })
-        }
-      )
+    fetch(
+      `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${path}`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        // 需要设置Content-Type 否则报401错误
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify({
+          access_token: accessToken,
+          content: content,
+          message: `uploadpic ${path}`
+        })
+      }
+    )
       .then(res => res.json())
       .then(res => {
         const download_url = res.content.download_url
@@ -43,6 +37,47 @@ ipcRenderer.on('toUploadPic', () => {
 
         clipboard.writeText(`![](${download_url})`)
       })
+  }
+}
+
+ipcRenderer.on('toUploadPic', () => {
+  const base64 = clipboard.readImage().toDataURL().split(',')[1]
+
+  if (base64) {
+    const path = `pic-${Date.now()}-${Math.random().toString(32)}.png`
+    let settings = localStorage.getItem('settings')
+
+    if (settings) {
+      settings = JSON.parse(settings)
     }
+    else {
+      settings = {
+        currentPicServer: 'gitee',
+        customLinkFormat: '![]($url)'
+      }
+    }
+
+    const config = localStorage.getItem(settings.currentPicServer)
+
+    if (!config) {
+      alert('没有配置Pic服务')
+      return
+    }
+
+    picServer[settings.currentPicServer](
+      JSON.parse(config),
+      base64,
+      path
+    ).then(url => {
+
+      ipcRenderer.send('message', {
+        title: 'Pic',
+        body: '图片上传成功!'
+      })
+
+      clipboard.writeText(
+        settings.customLinkFormat.replace('$url', url)
+      )
+    })
   }
 })
