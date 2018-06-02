8263fdb25d0a7a0453a3ecbd9a2d0096bd1abda4
爬上上坡 <pashangshangpo@gmail.com>
2018-06-02 14:48:51 +0800

    上传图片

--- src/page/index/router/index/uploadPic.js
+++ src/page/index/router/index/uploadPic.js
@@ -7,5 +7,35 @@
 import { clipboard, ipcRenderer } from 'electron'
 
 ipcRenderer.on('toUploadPic', () => {
-  console.log(clipboard.readImage().toDataURL().split(',')[1])
+  const base64 = clipboard.readImage().toDataURL().split(',')[1]
+  const gitee = localStorage.getItem('gitee')
+
+  if (gitee) {
+    const { accessToken, owner, repo } = JSON.parse(gitee)
+
+    if (base64) {
+      const path = `pic-${Date.now()}-${Math.random().toString(32)}.png`
+
+      fetch(
+        `https://gitee.com/api/v5/repos/${owner}/${repo}/contents/${path}`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          // 需要设置Content-Type 否则报401错误
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify({
+            access_token: accessToken,
+            content: base64,
+            message: `uploadpic ${path}`
+          })
+        }
+      )
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+      })
+    }
+  }
 })
