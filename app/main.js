fd8fe152b91a0e9cbd1033148f2f2868bffb104d
爬上上坡 <pashangshangpo@gmail.com>
2018-06-03 12:38:46 +0800

    修改配置

--- app/main.js
+++ app/main.js
@@ -17,6 +17,9 @@ function createWindow() {
     center: true,
     y: 60,
     show: false,
+    maximizable: false,
+    closable: false,
+    resizable: false,
     fullscreenable: true,
     /**
      * webSecurity Boolean (可选) - 当设置为 false, 它将禁用同源策略 (通常用来测试网站), 如果此选项不是由开发者设置的，还会把 allowRunningInsecureContent设置为 true. 默认值为 true。
